7951046e981fc05a8324113b5f7bf3262b244a8a
VinhTranHuu <vinh.tranhuu@ncc.asia>
2025-03-21 17:36:56 +0700

    test

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/google-font-display */
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="application-name" content="OneSignal React Sample" />
+        <meta
+          name="description"
+          content="OneSignal React integration with Next.js"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Demo from "@components/Demo"
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>OneSignal + Next.js 13</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="container mx-auto sm:px-6 lg:px-8 mt-4 mb-4 h-screen flex flex-col gap-4">
+        <h1 className="text-3xl text-center">OneSignal Next.js 13</h1>
+
+        <Demo />
+      </main>
+    </>
+  )
+}
